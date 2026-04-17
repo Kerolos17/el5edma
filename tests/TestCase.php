1dd37a62b9feb7caa299e3f2d262e9c79eadca22
Kerolos Morkos <135118128+Kerolos17@users.noreply.github.com>
2026-04-17 15:45:04 +0200

    fix: add fake Vite manifest step to CI for view rendering tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,13 +3,8 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Illuminate\Support\Facades\Vite;
 
 abstract class TestCase extends BaseTestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-        Vite::fake();
-    }
+    //
 }
