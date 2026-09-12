3471752758990b82c182e63ab88375fc6a07a6be
Kerolos Morkos <135118128+Kerolos17@users.noreply.github.com>
2026-09-12 21:19:31 +0300

    fix: preserve personal code uniqueness on mysql

--- tests/Feature/ServiceLeaderSensitiveResourceScopeTest.php
+++ tests/Feature/ServiceLeaderSensitiveResourceScopeTest.php
@@ -22,7 +22,7 @@ class ServiceLeaderSensitiveResourceScopeTest extends TestCase
     {
         $leader = $this->createServiceLeader();
         $managedGroup = ServiceGroup::factory()->create(['service_leader_id' => $leader->id]);
-        $otherGroup   = ServiceGroup::factory()->create();
+        $otherGroup = ServiceGroup::factory()->create();
 
         $managedBeneficiary = Beneficiary::factory()->create([
             'service_group_id' => $managedGroup->id,
@@ -53,8 +53,8 @@ class ServiceLeaderSensitiveResourceScopeTest extends TestCase
         ]);
 
         $medicalPolicy = new MedicalFilePolicy;
-        $prayerPolicy  = new PrayerRequestPolicy;
-        $medPolicy     = new MedicationPolicy;
+        $prayerPolicy = new PrayerRequestPolicy;
+        $medPolicy = new MedicationPolicy;
 
         $this->assertTrue($medicalPolicy->view($leader, $managedMedicalFile));
         $this->assertTrue($medicalPolicy->delete($leader, $managedMedicalFile));
