39ef3f7e3d99e34760142eb7e3786a0ade8f17b0
Kerolos Morkos <135118128+Kerolos17@users.noreply.github.com>
2026-09-12 21:32:28 +0300

    test: scope visit deletion to managed service groups

--- tests/Feature/AuthorizationTest.php
+++ tests/Feature/AuthorizationTest.php
@@ -123,8 +123,8 @@ class AuthorizationTest extends TestCase
         $superAdmin    = User::factory()->create(['role' => 'super_admin']);
         $serviceLeader = User::factory()->create(['role' => 'service_leader']);
         $group->update(['service_leader_id' => $serviceLeader->id]);
-        $familyLeader  = User::factory()->create(['role' => 'family_leader', 'service_group_id' => $group->id]);
-        $servant       = User::factory()->create(['role' => 'servant', 'service_group_id' => $group->id]);
+        $familyLeader = User::factory()->create(['role' => 'family_leader', 'service_group_id' => $group->id]);
+        $servant      = User::factory()->create(['role' => 'servant', 'service_group_id' => $group->id]);
 
         $beneficiary = Beneficiary::factory()->create(['service_group_id' => $group->id]);
         $visit       = Visit::factory()->create(['beneficiary_id' => $beneficiary->id]);
