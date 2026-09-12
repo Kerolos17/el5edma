0f3a1e18fdc40c9f401b4dde002773179fa1dc21
Kerolos Morkos <135118128+Kerolos17@users.noreply.github.com>
2026-09-12 17:05:01 +0300

    fix: associate modal dialogs with visible titles

--- resources/views/components/web-app/modal.blade.php
+++ resources/views/components/web-app/modal.blade.php
@@ -2,17 +2,27 @@
     'show' => false,
     'wide' => false,
     'title' => '',
+    'titleId' => null,
     'description' => '',
     'close' => '',
 ])
 @if ($show)
     <div class="app-modal-backdrop" wire:click="{{ $close }}" aria-hidden="true"></div>
-    <section class="app-modal-sheet" role="dialog" aria-modal="true" aria-label="{{ $title }}">
+    <section
+        class="app-modal-sheet"
+        role="dialog"
+        aria-modal="true"
+        @if ($titleId)
+            aria-labelledby="{{ $titleId }}"
+        @else
+            aria-label="{{ $title }}"
+        @endif
+    >
         <div class="app-modal-panel{{ $wide ? ' app-modal-panel-wide' : '' }}" tabindex="-1">
             <div class="app-modal-header">
                 <div>
                     @if ($description)<p class="app-section-label">{{ $description }}</p>@endif
-                    <h3>{{ $title }}</h3>
+                    <h3 @if ($titleId) id="{{ $titleId }}" @endif>{{ $title }}</h3>
                 </div>
                 <button type="button" wire:click="{{ $close }}" class="app-icon-button" aria-label="{{ __('web_app.actions.close') }}">
                     <i class="ph ph-x" aria-hidden="true"></i>
